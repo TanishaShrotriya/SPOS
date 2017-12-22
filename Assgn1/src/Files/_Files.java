f66a815c544e9758a9610ce1f58ebc5dedb4e877
ccoew <ccoew@localhost.localdomain>
2017-12-22 10:06:16 +0530

    separating to files, space issue

--- Assgn1/src/Files/_Files.java
+++ Assgn1/src/Files/_Files.java
@@ -16,15 +16,56 @@ public static String readFileAsString(String fileName)throws Exception
 
 public static void main(String[] args) throws Exception
 {
- String data = readFileAsString("/home/ccoew/3476/Assgn1/src/one.txt");
- String[] s = data.split("[\\W\\s]");
+ String data = readFileAsString("/home/ccoew/3476-master/Assgn1/src/one.txt");
+ System.out.print(data);
+ String[] s = data.split("\\W");
 
- for(String w :s) {
-	 System.out.print(w);
-
-	 System.out.print("\n");
+ 
+ ArrayList<String> str = new ArrayList<String>();
+ for(String e : s){
+	 str.add(e);
  }
+ System.out.print(str.toString());
+ int counter = 3;
+	
+ for(String w :str) {
+	 if(!w.equals(" ")) {
+	 if(w.matches("\\d*")||w.matches("\\D")||w.matches(".REG+")&&counter!=3)
+	 { 
+		 System.out.print(w+" ");
 
+		 if(counter==1){
+			 System.out.print(counter+" ");
+		     counter=3;				 
+		 }
+
+		 if(counter==2){
+			 System.out.print(counter+" ");
+		     counter--;				 
+		 }
+
+		 
+	 }
+	 else {
+		 
+
+		 System.out.print(w+" ");
+	     
+		 if(counter==1){
+			 
+			 System.out.print(counter+" ");
+		     counter=2;				 
+		 }
+
+		 else if(counter==3){
+			 System.out.println(counter+" ");
+		     counter--;				 
+		 }
+	 }
+	 
+	 }
+ }
+ 
 }
 
 }
